8254d8b3cb98b22bad10e57bd112567be157233a
Jabez Villamarzo <simplesimmering@gmail.com>
2023-08-28 21:16:14 +0800

    Add add(), subtract(), multiply(), and divide()

---
+++ script.js
@@ -0,0 +1,27 @@
+function add(num1, num2) {
+    const number1 = parseInt(num1);
+    const number2 = parseInt(num2);
+
+    return number1 + number2;
+}
+
+function subtract(num1, num2) {
+    const number1 = parseInt(num1);
+    const number2 = parseInt(num2);
+    
+    return number1 - number2;
+}
+
+function multiply(num1, num2) {
+    const number1 = parseInt(num1);
+    const number2 = parseInt(num2);
+    
+    return number1 * number2;
+}
+
+function divide(num1, num2) {
+    const number1 = parseInt(num1);
+    const number2 = parseInt(num2);
+    
+    return number1 / number2;
+}
